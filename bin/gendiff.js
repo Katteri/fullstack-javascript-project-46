16157bfb24232267b2b8e7f8d6364ee657ba94ee
Ekaterina Domashova <katy.domashova@gmail.com>
2025-01-12 18:37:37 +0300

    add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
