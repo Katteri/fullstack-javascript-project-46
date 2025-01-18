f926de608e3c4ed69970a917a6e90ef0119f201c
Ekaterina Domashova <katy.domashova@gmail.com>
2025-01-18 14:21:10 +0300

    add json formatter

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -13,15 +13,27 @@ const extensions = ['json', 'yml'];
 const correct = {
   stylish: readFile('correct-stylish.txt'),
   plain: readFile('correct-plain.txt'),
+  json: readFile('correct-json.txt'),
 };
 
-extensions.forEach((ext) => {
-  describe('Positives cases', () => {
-    test(`genDiff-${ext}`, () => {
+describe('Positives cases', () => {
+  extensions.forEach((ext) => {
+    test(`Format ${ext.toUpperCase()}`, () => {
       const file1 = getFixturePath(`file1.${ext}`);
       const file2 = getFixturePath(`file2.${ext}`);
       expect(genDiff(file1, file2)).toEqual(correct.stylish);
       expect(genDiff(file1, file2, 'plain')).toEqual(correct.plain);
+      expect(genDiff(file1, file2, 'json')).toEqual(correct.json);
     });
+  });
+});
+
+describe('Negative cases', () => {
+  test('Check wrong file extension', () => {
+    const error = new Error("Invalid file extension: 'txt'! Try supported formats.");
+
+    expect(() => {
+      genDiff(getFixturePath('file1-wrong.txt'), getFixturePath('file2-wrong.txt'));
+    }).toThrow(error);
   });
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import makeStylish from './stylish.js';
 import makePlain from './plain.js';
+import makeJSON from './json.js';
 
 function formattedTree(data, format, replacer) {
   switch (format) {
@@ -7,6 +8,8 @@ function formattedTree(data, format, replacer) {
       return makeStylish(data, replacer);
     case 'plain':
       return makePlain(data);
+    case 'json':
+      return makeJSON(data);
     default:
       throw new Error(`Invalid file format type: '.${format}'! Try supported file formats.`);
   }
